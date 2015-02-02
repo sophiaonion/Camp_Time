fe33e0a8e7ec20fe8ef4640e09b318bc5bfe1430
Eric <egarasky@gmail.com>
2015-02-02 15:23:18 -0600

    add_registration.js get sessions from resource

--- src/main/webapp/scripts/add_registration.js
+++ src/main/webapp/scripts/add_registration.js
@@ -1,9 +1,20 @@
-var main = function(){
+var main = function(camp_sessions){
+    //get camper name from linked camper registration page
+    var qs = new QueryString();
+
+    camp_sessions.forEach(function(session){
+        var element = $("<option>");
+        element.html('value', session.name);
+        element.data('sessionID', session.sessionID);
+    });
+
     $('#submit-registration').on('click', function(){
     var data = {
         camperID: $('#camper-name').val(),
         sessionID: $('#session-name').val()
     };
+
+
 
     $.ajax({
         type: 'PUT',
@@ -12,11 +23,14 @@ var main = function(){
         contentType: 'application/JSON'
     });
 
-    });
+    $('#camper-name').val("");
+    $('#session-name').val("");
+});
 
-}
+};
 //eventually get campers and sessions and pass to main function
 $(document).ready(function(){
-
-
+        $.get('/api/campsessions', function(camp_sessions){
+            main(campSessions);
+        });
 });
