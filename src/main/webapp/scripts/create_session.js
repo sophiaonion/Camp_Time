7420314a3c7b9ed4e77747cb72903c70afd21853
Eric <egarasky@gmail.com>
2015-02-06 16:58:02 -0600

    schedule table builds

--- src/main/webapp/scripts/create_session.js
+++ src/main/webapp/scripts/create_session.js
@@ -1,5 +1,69 @@
 var main = function(camp_sessions){
-$("#schedule").hide();
+//get the format we want and correct time zone for date String
+Date.prototype.myToString = function(){
+    var utcDate = this.toUTCString(); //returns correct date as Day, Date Month Year time
+    utcDate = utcDate.slice(0, utcDate.indexOf('2015') - 1);
+    utcDate = utcDate.replace(',', '');
+    var pieces = utcDate.split(' '); //Day, Date, Month
+    return pieces[0] + ' ' + pieces[2] + ' ' + pieces[1]
+}
+
+//from stack overflow utility function for date difference
+//http://stackoverflow.com/questions/3224834/get-difference-between-2-dates-in-javascript
+// a and b are javascript Date objects
+function dateDiffInDays(a, b) {
+  // Discard the time and time-zone information.
+  var _MS_PER_DAY = 1000 * 60 * 60 * 24; //milliseconds per day
+  var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate()); //UTC must be used so timezones don't interfere...
+  var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+//set build table function to trigger when both end and start dates are set
+(function(){
+    var startDate, endDate; //following functions will be able to directly access
+    
+    var buildTable = function(){ //constructs schedule table
+        sDate = new Date(startDate);
+        var daysOfSession = dateDiffInDays(sDate, new Date(endDate)) + 1; //+1 include last day in for loop then
+        var i = 0;
+
+        for(i; i<daysOfSession; i++){
+            var dateHead = $('<td>').text(sDate.myToString()); //create date cell to add
+            dateHead.attr('id', '0' + i.toString()); //id will always be 0dayofcamp
+            $('#dates').append(dateHead);
+            var toClone = $('.clone'); //get clone column
+            console.log('loop');
+            toClone.each(function(index){
+                console.log($(this));
+                var next = $(this).clone(true);
+                next.removeClass('clone'); //otherwise it will be included in elements to clone next for loop iteration
+                var cloneId = $('input', this).attr('id');
+                next.attr('id', cloneId + i.toString());
+                $(this).parent().append(next);
+            }); //end forEach cloning loop
+            sDate.setDate(sDate.getDate() + 1);
+        } //end for loop through dates
+    }; //end build table function
+
+
+
+    $('#start-date').on('change', function(){
+        startDate = $(this).val();
+        if (startDate && endDate){//if startDate or endDate is null will evaluate to false
+            buildTable();
+        }
+    });
+
+    $('#end-date').on('change', function(){
+        endDate = $(this).val();
+        if (startDate && endDate){
+            buildTable();
+        }
+
+    });
+}()); //end closure, keeps endDate, startDate variables contained within the scope of function
 
     $('#enter-dates').on('click', function(){
         //print out schedule decider for fixed time activities
