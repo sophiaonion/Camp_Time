152ed0f2976a5af6eb20fcc835515efc01c5dcaa
Rachel Feltes <rachelleighfeltes@gmail.com>
2015-02-16 16:52:12 -0600

    checking ratio constraints

---
+++ src/main/java/camptimetest/domain/StaffConstraintChecker.java
@@ -0,0 +1,190 @@
+package camptimetest.domain;
+
+import com.mongodb.BasicDBList;
+import restx.jongo.JongoCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import restx.jongo.JongoCollection;
+import org.bson.types.ObjectId;
+
+import java.util.*;
+/**
+ * Created by rachelfeltes on 2/14/15.
+ */
+
+
+public class StaffConstraintChecker {
+
+    private JongoCollection activities;
+    private JongoCollection employees;
+    private JongoCollection registrations;
+    private JongoCollection campsessions;
+
+
+    public StaffConstraintChecker(JongoCollection activities, JongoCollection employees, JongoCollection registrations, JongoCollection campsessions) {
+        this.activities = activities;
+        this.employees = employees;
+        this.registrations = registrations;
+        this.campsessions = campsessions;
+    }
+
+    public JongoCollection update() {
+        System.out.println("updating now...");
+
+        //staffing activities
+        int i = this.checkConflicts();
+        while(this.checkConflicts() > 0) {
+            System.out.println("type "+i+" conflict");
+            break; //just here while testing
+        }
+        return this.activities;
+    }//end update()
+
+    private int checkConflicts() {
+        System.out.println("checking conflicts...");
+
+
+        DBCursor cursor = activities.get().getDBCollection().find();
+        List<DBObject> arrry = cursor.toArray();
+        ArrayList<DBObject> list = new ArrayList<DBObject>(arrry);
+
+        //if there is an activity without employees
+        if( (activities.get().count( "{employees: {$exists: false} }" ) != 0)
+                || (activities.get().count( "{employees: null }" ) != 0) ) {//if time is set or not
+            System.out.println("type 1 conflict found (no staff)");
+            return 1;
+        }
+
+        //make sure employees aren't working two activities at the same time
+
+
+        //for each activity make sure has appropriate number of employees
+        for(int i=0; i<list.size(); i++) {
+        //get necessary facts to use for the checking of constraints:
+            //get number of campers in session
+            String session = String.valueOf(list.get(i).get("session")); //get session name
+            String sessionID = campsessions.get().findOne("{name: # }", session).as(CampSession.class).getSessionID();
+            ObjectId ID = new ObjectId(sessionID);
+            int numCampers = (int) registrations.get().count("{sessionID: #}", ID); //get session id from that
+
+            //get age of campers in session
+            String age = String.valueOf(campsessions.get().findOne("{_id: # }", ID).as(CampSession.class).getAgeGroup());
+
+            //get location of activity
+            String activityArea = String.valueOf(list.get(i).get("activityArea"));
+
+            //get number of employees working session
+            int numStaff = 0;
+            BasicDBList e = null;
+            if (list.get(i).get("employees") != null) {//can maybe get rid of this laterzzz
+                e = (BasicDBList) list.get(i).get("employees");
+                numStaff = e.size();
+            } else System.out.println("is empty");//return 1;//no employees scheduled yet
+
+
+        //begin constraint checking
+            //at least 2 staff are with campers
+            if ((numStaff < 2)) {System.out.println("not enough staff"); return 2;}
+
+            //woman over 18 is with campers
+            boolean hasMatureLady = false;
+            for (int j = 0; j < e.size(); j++) {
+                if (((Employee) e.get(j)).getGender().equals("woman") && ((Employee) e.get(j)).getAge() >= 18) {
+                    hasMatureLady = true;
+                }
+            }
+            if (hasMatureLady == false) {
+                System.out.println("does not have staff over 18 and woman");
+                return 4;
+            }
+
+            //check that activities have correct ratios
+            switch (age) {
+                case "daisy": //need 2 staff for first 6 campers, then 1 staff for every 4 campers after that
+                    if(numCampers<= 6) {if(numStaff < 2) {return 5;}}//need 2 staff for first 6 campers
+                    else if (numStaff == 2) {return 5;}//separate this instance so won't divide by 0 - more than 6 campers but only 2 staff
+                    else if ( (numCampers - 6) / (numStaff - 2) >= 4) {return 5;}//need 1 staff for every 4 campers after that
+                    break;
+                case "brownie":
+                    if (numCampers / numStaff >= 6) {return 5;}
+                    break;
+                case "junior":
+                    if (numCampers / numStaff >= 8) {return 5;}
+                    break;
+                case "cadette":
+                    if (numCampers / numStaff >= 10) {return 5;}
+                    break;
+                case "senior":
+                    if (numCampers / numStaff >= 12) {return 5;}
+                    break;
+                case "ambassador":
+                    if (numCampers / numStaff >= 12) {return 1;}
+                    break;
+                default:
+                    System.out.println("error: no age group assigned");
+                    return -1;
+            }
+
+            //ensure that areas requiring certifications have those certifications
+            if (list.get(i).get("activityArea") != null) {
+                int numLifeGuards = 0;
+                boolean hasArt, hasNature, hasArchery, hasStore, canDrive;
+                hasArt=false; hasNature=false; hasArchery=false; hasStore=false; canDrive=false;
+
+                //see what certifications activity has
+                for(int j=0; j<e.size(); j++) {
+                    ArrayList<String> certs = new ArrayList<String> (((Employee) e.get(j)).getCertifications());
+                    for(int k=0; k<certs.size(); k++) {
+                        if(certs.get(k).equals("lifeguard")) {numLifeGuards++;}
+                        if(certs.get(k).equals("art")) {hasArt=true;}
+                        if(certs.get(k).equals("nature")) {hasNature=true;}
+                        if(certs.get(k).equals("archery")) {hasArchery=true;}
+                        if(certs.get(k).equals("store")) {hasStore=true;}
+                        if(certs.get(k).equals("drive")) {canDrive=true;}
+                    }//end check certifications for employee
+                }//end check certifications for all employees in activity
+
+
+                //based on activity area see if necessary certifications are present
+                boolean certsOK = false;
+                switch(activityArea) {
+                    case "pool":
+                        if ((((numCampers + numStaff) / numLifeGuards) < 25) && (((numCampers + numStaff) / (numStaff - ((numCampers + numStaff) / 25))) >= 12)) {certsOK = true;}
+                        break;
+                    case "canoeing":
+                        if ((((numCampers + numStaff) / numLifeGuards) < 25) && (((numCampers + numStaff) / (numStaff - ((numCampers + numStaff) / 25))) >= 12)) {certsOK = true;}
+                        break;
+                    case "art":
+                        if(hasArt) {certsOK = true;}
+                        break;
+                    case "nature":
+                        if(hasNature) {certsOK = true;}
+                        break;
+                    case "archery":
+                        if(hasArchery) {certsOK = true;}
+                        break;
+                    case "store":
+                        if(hasStore) {certsOK = true;}
+                        break;
+                    default:
+                        System.out.println("error: somethin funky going on");
+                }
+                if (!certsOK) {
+                    System.out.println("lacking required certifications");
+                    return 6;
+                }
+            }//end check for required certifications
+        }//end check for correct ratios
+
+        //check that employees aren't schedule multiple places at same time todo
+
+        //check that employees have appropriate number of breaks todo
+
+        System.out.println("checked conflicts");
+        return 0;
+    }
+
+}
--- target/generated-sources/annotations/camptimetest/rest/ActivityResourceFactoryMachine.java
+++ target/generated-sources/annotations/camptimetest/rest/ActivityResourceFactoryMachine.java
@@ -10,12 +10,16 @@ public class ActivityResourceFactoryMachine extends SingleNameFactoryMachine<Act
 
     public ActivityResourceFactoryMachine() {
         super(0, new StdMachineEngine<ActivityResource>(NAME, BoundlessComponentBox.FACTORY) {
+private final Factory.Query<restx.jongo.JongoCollection> registrations = Factory.Query.byName(Name.of(restx.jongo.JongoCollection.class, "registrations")).mandatory();
+private final Factory.Query<restx.jongo.JongoCollection> employees = Factory.Query.byName(Name.of(restx.jongo.JongoCollection.class, "employees")).mandatory();
 private final Factory.Query<restx.jongo.JongoCollection> activities = Factory.Query.byName(Name.of(restx.jongo.JongoCollection.class, "activities")).mandatory();
 private final Factory.Query<restx.jongo.JongoCollection> campsessions = Factory.Query.byName(Name.of(restx.jongo.JongoCollection.class, "campsessions")).mandatory();
 
             @Override
             public BillOfMaterials getBillOfMaterial() {
                 return new BillOfMaterials(ImmutableSet.<Factory.Query<?>>of(
+registrations,
+employees,
 activities,
 campsessions
                 ));
@@ -24,6 +28,8 @@ campsessions
             @Override
             protected ActivityResource doNewComponent(SatisfiedBOM satisfiedBOM) {
                 return new ActivityResource(
+satisfiedBOM.getOne(registrations).get().getComponent(),
+satisfiedBOM.getOne(employees).get().getComponent(),
 satisfiedBOM.getOne(activities).get().getComponent(),
 satisfiedBOM.getOne(campsessions).get().getComponent()
                 );
